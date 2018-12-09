0ac3f52f2e5d2729fd17b4be5ec2dabc624c4400
Zeki Esenalp <zekiesenalp@github.com>
2018-12-09 21:16:01 +0300

    first commit

---
+++ controllers/api/DefaultController.php
@@ -0,0 +1,18 @@
+<?php
+namespace kouosl\iletisim\controllers\api;
+
+
+/**
+ * Default controller for the `iletisim` module
+ */
+class DefaultController extends \kouosl\base\controllers\api\BaseController
+{
+    /**
+     * Renders the index view for the module
+     * @return string
+     */
+    public function actionIndex()
+    {
+        return $this->render('_index');
+    }
+}
---
+++ controllers/console/DefaultController.php
@@ -0,0 +1,18 @@
+<?php
+namespace kouosl\iletisim\controllers\console;
+
+
+/**
+ * Default controller for the `iletisim` module
+ */
+class DefaultController extends \kouosl\base\controllers\console\BaseController
+{
+    /**
+     * Renders the index view for the module
+     * @return string
+     */
+    public function actionIndex()
+    {
+        return $this->render('_index');
+    }
+}
