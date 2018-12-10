9593d0d35c5b63bfc5b480b888df2382da92079f
Zeki Esenalp <zekiesenalp@github.com>
2018-12-10 16:35:59 +0300

    modül denemesi yapıldı.

--- views/frontend/default/index.php
+++ views/frontend/default/index.php
@@ -2,9 +2,9 @@
 <div class="site-index">
 
     <div class="jumbotron">
-        <h1>Kou Osl Yii2 App</h1>
+        <h1>iletisim'den modül denemesi. push</h1>
 
-        <p class="lead">Örnek uygulamayı başarılı bir şekilde çalıştırdınız.</p>
+        <p class="lead">modül denemesi.</p>
 
         <p><a class="btn btn-lg btn-success" href="#">Modüller ve konfürgasyon!</a></p>
     </div>
